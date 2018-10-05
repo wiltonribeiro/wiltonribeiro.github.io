117a9f1c1491b885b626a5766aaa48bc7f066407
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-10-05 19:17:59 -0300

    Initial commit

---
+++ precache-manifest.d5a5dfb21c8030eee48154ff66297c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e6bdb0e44f1ab053665",
+    "url": "/static/js/main.6e6bdb0e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6e6bdb0e44f1ab053665",
+    "url": "/static/css/main.1b12ded2.chunk.css"
+  },
+  {
+    "revision": "d6a9a415d653e2e2a3ce445bee559679",
+    "url": "/index.html"
+  }
+];
